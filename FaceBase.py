c774f89548b03cc48a09c32118ac5169950d4d9e
Muizzul Rizqon Gunawan <150110295+Muizzul24@users.noreply.github.com>
2025-01-14 02:30:13 +0700

    Update FaceBase.py

--- FaceBase.py
+++ FaceBase.py
@@ -60,20 +60,3 @@ def verify_user(hog_features):
 
     return 'unknown_person'
 
-# Fungsi untuk menghubungkan ke database MySQL
-def connect_to_database():
-    try:
-        conn = mysql.connector.connect(
-            host="100.110.248.65",  # Hostname atau IP server MySQL
-            port=3306,                           # Port MySQL
-            user="root",                         # Username MySQL
-            password="04207027",                 # Password MySQL
-            database="facebase",   
-            ssl_disabled=False              # Nama database MySQL
-        )
-        if conn.is_connected():
-            st.success("Berhasil terhubung ke database!")
-            return conn
-    except Error as err:
-        st.error(f"Kesalahan koneksi database: {err}")
-        return None
